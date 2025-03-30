a5d9136c51d45d505a7ef15fb70a3e3ab372e178
charlyoleg <charles.braquet@gmx.fr>
2025-03-30 18:07:30 +0200

    a dummy unit test

---
+++ pkg/desi77/src/stone/door1arc.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+//import * from './door1arc'
+
+describe('sum test', () => {
+	it('adds 1 + 2 to equal 3', () => {
+		expect(1 + 2).toBe(3);
+	});
+});
