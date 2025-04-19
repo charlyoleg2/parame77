e72b28363f65ce9c0e7d65ada6b06ca72a79485e
charlyoleg <charles.braquet@gmx.fr>
2025-04-19 14:30:59 -0400

    torque holes

--- pkg/desi77/src/spring/plugTorque.ts
+++ pkg/desi77/src/spring/plugTorque.ts
@@ -2,7 +2,7 @@
 // plugable torque transmission
 
 import type {
-	//tContour,
+	tContour,
 	//tOuterInner,
 	tParamDef,
 	tParamVal,
@@ -14,13 +14,13 @@ import type {
 import {
 	//withinZeroPi,
 	//ShapePoint,
-	//point,
+	point,
 	//contour,
 	contourCircle,
 	//ctrRectangle,
 	figure,
 	//degToRad,
-	//radToDeg,
+	radToDeg,
 	ffix,
 	pNumber,
 	//pCheckbox,
@@ -64,7 +64,7 @@ const pDef: tParamDef = {
 		pNumber('DTi', 'mm', 3, 0.1, 100, 0.1),
 		pNumber('Ei', 'mm', 2, 0.1, 100, 0.1),
 		pSectionSeparator('External part'),
-		pNumber('Ne', 'holes', 5, 0, 1000, 1),
+		pNumber('Ne', 'holes', 8, 0, 1000, 1),
 		pNumber('De', 'mm', 50, 0.1, 1000, 0.1),
 		pNumber('DTe', 'mm', 3, 0.1, 100, 0.1),
 		pNumber('Ee', 'mm', 2, 0.1, 100, 0.1)
@@ -108,31 +108,88 @@ const pDef: tParamDef = {
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
 	const figIntern = figure();
-	//const figExtern = figure();
+	const figExtern = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
 		const Rt = param.Dt / 2;
+		const Ri = param.Di / 2;
+		const Re = param.De / 2;
+		const RTi = param.DTi / 2;
+		const RTe = param.DTe / 2;
+		const RminI = Ri - RTi - param.Ei;
+		const RmaxE = Re + RTe + param.Ee;
+		const adTi = param.Ni > 0 ? (2 * Math.PI) / param.Ni : 3.14;
+		const adTe = param.Ne > 0 ? (2 * Math.PI) / param.Ne : 3.14;
+		const aMindTi = Math.asin((RTi + 0.3 * param.Ei) / Ri);
+		const aMindTe = Math.asin((RTe + 0.3 * param.Ee) / Re);
 		// step-5 : checks on the parameter values
-		if (Rt < 0) {
-			throw `err087: Dt ${param.Dt} is too small compare to Nt ${param.Nt}`;
+		if (RminI < 0.1) {
+			throw `err087: RminI ${RminI} is too small because of Di ${param.Di}, DTi ${param.DTi} or Ei ${param.Ei}`;
+		}
+		if (adTi < 2 * aMindTi) {
+			throw `err131: adTi ${ffix(radToDeg(adTi))} is too small compare to aMindTi ${ffix(radToDeg(aMindTi))} degree`;
+		}
+		if (adTe < 2 * aMindTe) {
+			throw `err134: adTe ${ffix(radToDeg(adTe))} is too small compare to aMindTe ${ffix(radToDeg(aMindTe))} degree`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `Extern: Dmax ${ffix(2 * Rt)} mm\n`;
+		rGeome.logstr += `Extern: Dmax ${ffix(2 * RmaxE)}, Dmin mm\n`;
+		rGeome.logstr += `Intern: Dmax, Dmin ${ffix(2 * RminI)} mm\n`;
 		// sub-function
+		// Intern
+		const ctrsI: tContour[] = [];
+		ctrsI.push(contourCircle(0, 0, RminI));
+		for (let ii = 0; ii < param.Ni; ii++) {
+			const p1 = point(0, 0).translatePolar(ii * adTi, Ri);
+			ctrsI.push(contourCircle(p1.cx, p1.cy, RTi));
+		}
+		// Extern
+		const ctrsE: tContour[] = [];
+		ctrsE.push(contourCircle(0, 0, RmaxE));
+		for (let ii = 0; ii < param.Ne; ii++) {
+			const p1 = point(0, 0).translatePolar(ii * adTe, Re);
+			ctrsE.push(contourCircle(p1.cx, p1.cy, RTe));
+		}
 		// figIntern
-		figIntern.addMainO(contourCircle(0, 0, Rt));
+		figIntern.addMainOI([contourCircle(0, 0, Rt), ...ctrsI]);
+		// figExtern
+		figIntern.addMainOI([...ctrsE, contourCircle(0, 0, Rt)]);
 		// final figure list
 		rGeome.fig = {
-			faceIntern: figIntern
+			faceIntern: figIntern,
+			faceExtern: figExtern
 		};
 		// volume
 		const designName = rGeome.partName;
+		const nameSubpaxIntern = `subpax_${designName}_intern`;
+		const nameSubpaxExtern = `subpax_${designName}_extern`;
+		const nameList: string[] = [];
+		switch (param.make3D) {
+			case 1:
+				nameList.push(nameSubpaxIntern);
+				break;
+			case 2:
+				nameList.push(nameSubpaxExtern);
+				break;
+			default:
+				nameList.push(nameSubpaxIntern);
+				nameList.push(nameSubpaxExtern);
+				break;
+		}
 		rGeome.vol = {
 			extrudes: [
 				{
-					outName: `subpax_${designName}_intern`,
+					outName: nameSubpaxIntern,
 					face: `${designName}_faceIntern`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: param.Th,
+					rotate: [0, 0, 0],
+					translate: [0, 0, 0]
+				},
+				{
+					outName: nameSubpaxExtern,
+					face: `${designName}_faceExtern`,
 					extrudeMethod: EExtrude.eLinearOrtho,
 					length: param.Th,
 					rotate: [0, 0, 0],
@@ -142,8 +199,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			volumes: [
 				{
 					outName: `pax_${designName}`,
-					boolMethod: EBVolume.eIdentity,
-					inList: [`subpax_${designName}_intern`]
+					boolMethod: EBVolume.eUnion,
+					inList: nameList
 				}
 			]
 		};
