60ba85b53a092b8ba7e2eb4ac1f3ef799114a32b
charlyoleg <charles.braquet@gmx.fr>
2025-04-23 23:25:18 -0400

    design with arc wip

--- pkg/desi77/src/spring/springTorqueC.ts
+++ pkg/desi77/src/spring/springTorqueC.ts
@@ -3,6 +3,7 @@
 
 import type {
 	Point,
+	Contour,
 	tContour,
 	//tOuterInner,
 	tParamDef,
@@ -202,7 +203,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		}
 		const pts0: Point[] = [];
 		const pts1: Point[] = [];
-		//const ptl: number[] = [];
+		const ptl: number[] = [];
 		const k2cos = Math.cos(aSpring);
 		const k2sin = Math.sin(aSpring);
 		const k2cx = Rk * k2sin;
@@ -211,7 +212,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		for (let ii = 0; ii < param.Nk; ii++) {
 			const k2Parity2 = param.zig === 1 ? 0 : 1;
 			const kl = Rsi + Esi + 2 * ii * Rk;
-			//ptl.push(kl);
+			ptl.push(kl);
 			if (ii % 2 === k2Parity1) {
 				pts0.push(point(kl, Rk));
 			} else {
@@ -236,9 +237,9 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			const a2 = iParity === 0 ? a1 : aSpring + a1;
 			return point(0, 0).translatePolar(a2, l2);
 		}
-		const eParity = param.Nk % 2;
+		const extParity = param.Nk % 2;
 		function externParity(iParity: number): number {
-			const eeParity = (eParity + iParity + 1) % 2;
+			const eeParity = (extParity + iParity + 1) % 2;
 			return eeParity;
 		}
 		function pppE(a1: number, iParity: number, l2: number): Point {
@@ -263,8 +264,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		ppp1.push(pppE(-a36, 1, Rse - Ese).rotateOrig(aSpringStep)); // 5
 		ppp1.push(pppI(-a27, 1, Rsi + Esi).rotateOrig(aSpringStep)); // 6
 		ppp1.push(pppI(-a18, 1, Rsi).rotateOrig(aSpringStep)); // 7
-		//const Rks = Rk - Wk2;
-		//const Rkl = Rk + Wk2;
+		const Rks = Rk - Wk2;
+		const Rkl = Rk + Wk2;
 		function iiParity(ii: number): [number, number] {
 			let ip0 = ii % 2;
 			let ip1 = ii < param.Ns - 1 ? (ii + 1) % 2 : 0;
@@ -330,93 +331,70 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			figProfile.addSecond(contourCircle(pt1.cx, pt1.cy, Rk));
 		}
 		// partial-1
-		//const ctrPartial1 = contour(pt1.cx, pt1.cy);
-		//for (let ii = 0; ii < param.Nk; ii++) {
-		//	let a11 = -Math.PI / 2;
-		//	//const a12 = Math.PI / 2;
-		//	//const a21 = -Math.PI / 2;
-		//	const [a12, a21] = calcAzig(pts1[ii].cx, pts2[ii].cx, Rks, Rkl);
-		//	let a22 = Math.PI / 2;
-		//	if (ii > 0) {
-		//		const [, tmpa11] = calcAzag(pts2[ii - 1].cx, pts1[ii].cx, Rkl, Rks);
-		//		a11 = tmpa11;
-		//	}
-		//	if (ii < param.Nk - 1) {
-		//		const [tmpa22] = calcAzag(pts2[ii].cx, pts1[ii + 1].cx, Rkl, Rks);
-		//		a22 = tmpa22;
-		//	}
-		//	const p11 = pts1[ii].translatePolar(a11, Rks);
-		//	const p1b = pts1[ii].translatePolar(a11 + withinZeroPi((a12 - a11) / 2), Rks);
-		//	const p12 = pts1[ii].translatePolar(a12, Rks);
-		//	const p21 = pts2[ii].translatePolar(a21, Rkl);
-		//	const p2b = pts2[ii].translatePolar(a21 - withinZeroPi((a21 - a22) / 2), Rkl);
-		//	const p22 = pts2[ii].translatePolar(a22, Rkl);
-		//	ctrPartial1
-		//		.addSegStrokeA(p11.cx, p11.cy)
-		//		.addPointA(p1b.cx, p1b.cy)
-		//		.addPointA(p12.cx, p12.cy)
-		//		.addSegArc2()
-		//		.addSegStrokeA(p21.cx, p21.cy);
-		//	if (ii < param.Nk - 1) {
-		//		ctrPartial1.addPointA(p2b.cx, p2b.cy).addPointA(p22.cx, p22.cy).addSegArc2();
-		//	}
-		//}
-		//const [, lasta21] = calcAzig(pts1[param.Nk - 1].cx, pts2[param.Nk - 1].cx, Rks, Rkl);
-		//const a2c = Math.PI / 2 + aSpring;
-		//const a2b = lasta21 - withinZeroPi((lasta21 - a2c) / 2);
-		//const pt2b = pts2[param.Nk - 1].translatePolar(a2b, Rkl);
-		//const pt2c = pts2[param.Nk - 1].translatePolar(a2c, Rkl);
-		//const pt3b = pts2[param.Nk - 1].translatePolar(a2b, Rks);
-		//const pt3c = pts2[param.Nk - 1].translatePolar(a2c, Rks);
-		//ctrPartial1
-		//	.addPointA(pt2b.cx, pt2b.cy)
-		//	.addPointA(pt2c.cx, pt2c.cy)
-		//	.addSegArc2()
-		//	.addSegStrokeA(pt2.cx, pt2.cy);
-		////figProfile.addSecond(ctrPartial1);
-		//// partial-2
-		//const [, firsta22] = calcAzig(pts1[param.Nk - 1].cx, pts2[param.Nk - 1].cx, Rkl, Rks);
-		//const firstp22 = pts2[param.Nk - 1].translatePolar(firsta22, Rks);
-		//const ctrPartial2 = contour(pt3.cx, pt3.cy)
-		//	.addSegStrokeA(pt3c.cx, pt3c.cy)
-		//	.addPointA(pt3b.cx, pt3b.cy)
-		//	.addPointA(firstp22.cx, firstp22.cy)
-		//	.addSegArc2();
-		//for (let ii = param.Nk - 1; ii >= 0; ii--) {
-		//	let a21 = Math.PI / 2;
-		//	//const a22 = -Math.PI / 2;
-		//	//const a11 = Math.PI / 2;
-		//	let a12 = -Math.PI / 2;
-		//	const [a11, a22] = calcAzig(pts1[ii].cx, pts2[ii].cx, Rkl, Rks);
-		//	if (ii > 0) {
-		//		const [, tmpa12] = calcAzag(pts2[ii - 1].cx, pts1[ii].cx, Rks, Rkl);
-		//		a12 = tmpa12;
-		//	}
-		//	if (ii < param.Nk - 1) {
-		//		const [tmpa21] = calcAzag(pts2[ii].cx, pts1[ii + 1].cx, Rks, Rkl);
-		//		a21 = tmpa21;
-		//	}
-		//	const p11 = pts1[ii].translatePolar(a11, Rkl);
-		//	const p1b = pts1[ii].translatePolar(a11 - withinZeroPi((a11 - a12) / 2), Rkl);
-		//	const p12 = pts1[ii].translatePolar(a12, Rkl);
-		//	const p21 = pts2[ii].translatePolar(a21, Rks);
-		//	const p2b = pts2[ii].translatePolar(a21 + withinZeroPi((a22 - a21) / 2), Rks);
-		//	const p22 = pts2[ii].translatePolar(a22, Rks);
-		//	if (ii < param.Nk - 1) {
-		//		ctrPartial2
-		//			.addSegStrokeA(p21.cx, p21.cy)
-		//			.addPointA(p2b.cx, p2b.cy)
-		//			.addPointA(p22.cx, p22.cy)
-		//			.addSegArc2();
-		//	}
-		//	ctrPartial2
-		//		.addSegStrokeA(p11.cx, p11.cy)
-		//		.addPointA(p1b.cx, p1b.cy)
-		//		.addPointA(p12.cx, p12.cy)
-		//		.addSegArc2();
-		//}
-		//ctrPartial2.addSegStrokeA(pt4.cx, pt4.cy);
-		////figProfile.addSecond(ctrPartial2);
+		function ctrPartial1(iParity: number): Contour {
+			const pts = iParity === 0 ? pts0 : pts1;
+			const aStart1 = iParity === 0 ? -Math.PI / 2 : Math.PI / 2 + aSpring;
+			const siRk = iParity === 0 ? Rks : Rkl;
+			const startPt1 = pts[0].translatePolar(aStart1, siRk);
+			const rCtr = contour(startPt1.cx, startPt1.cy);
+			for (let ii = 0; ii < param.Nk; ii++) {
+				const iiParity = (ii + iParity) % 2;
+				const aMid = iiParity === 0 ? -Math.PI / 2 : Math.PI / 2 + aSpring;
+				const aSign = iiParity === 0 ? 1 : -1;
+				const iRk = iiParity === 0 ? Rks : Rkl;
+				if (ii > 0) {
+					const p1 = point(0, 0).translatePolar(aSpring / 2, ptl[ii] - iRk);
+					const p2 = pts[ii].translatePolar(aMid - (aSign * Math.PI) / 2, iRk);
+					rCtr.addPointA(p1.cx, p1.cy).addPointA(p2.cx, p2.cy).addSegArc2();
+				}
+				let da3 = 0;
+				let da4 = Math.PI / 2;
+				if (ii === 0) {
+					da3 = Math.PI / 4;
+				} else if (ii === param.Nk - 1) {
+					da3 = -Math.PI / 4;
+					da4 = 0;
+				}
+				const p3 = pts[ii].translatePolar(aMid + aSign * da3, iRk);
+				const p4 = pts[ii].translatePolar(aMid + aSign * da4, iRk);
+				rCtr.addPointA(p3.cx, p3.cy).addPointA(p4.cx, p4.cy).addSegArc2();
+			}
+			return rCtr;
+		}
+		//figProfile.addSecond(ctrPartial1(0));
+		// partial-2
+		function ctrPartial2(iParity: number): Contour {
+			const pts = iParity === 0 ? pts0 : pts1;
+			const eParity = externParity(iParity);
+			const aStart1 = eParity === 0 ? -Math.PI / 2 : Math.PI / 2 + aSpring;
+			const siRk = eParity === 0 ? Rkl : Rks;
+			const startPt1 = pts[param.Nk - 1].translatePolar(aStart1, siRk);
+			const rCtr = contour(startPt1.cx, startPt1.cy);
+			for (let ii = param.Nk - 1; ii >= 0; ii--) {
+				const iiParity = (ii + iParity) % 2;
+				const aMid = iiParity === 0 ? -Math.PI / 2 : Math.PI / 2 + aSpring;
+				const aSign = iiParity === 0 ? -1 : 1;
+				const iRk = iiParity === 0 ? Rkl : Rks;
+				if (ii < param.Nk - 1) {
+					const p1 = point(0, 0).translatePolar(aSpring / 2, ptl[ii] + iRk);
+					const p2 = pts[ii].translatePolar(aMid - (aSign * Math.PI) / 2, iRk);
+					rCtr.addPointA(p1.cx, p1.cy).addPointA(p2.cx, p2.cy).addSegArc2();
+				}
+				let da3 = 0;
+				let da4 = Math.PI / 2;
+				if (ii === param.Nk - 1) {
+					da3 = Math.PI / 4;
+				} else if (ii === 0) {
+					da3 = -Math.PI / 4;
+					da4 = 0;
+				}
+				const p3 = pts[ii].translatePolar(aMid + aSign * da3, iRk);
+				const p4 = pts[ii].translatePolar(aMid + aSign * da4, iRk);
+				rCtr.addPointA(p3.cx, p3.cy).addPointA(p4.cx, p4.cy).addSegArc2();
+			}
+			return rCtr;
+		}
+		//figProfile.addSecond(ctrPartial2(0));
 		// ctrSpringHollow
 		function ctrSpringHollow(ii: number): tContour {
 			const [ip1, ip2] = iiParity(ii);
@@ -426,7 +404,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			const ctr = contour(pp1[0].cx, pp1[0].cy)
 				.addCornerRounded(param.Rrsi)
 				.addSegStrokeA(pp1[1].cx, pp1[1].cy)
-				.addSegStrokeA(pp1[2].cx, pp1[2].cy)
+				.addPartial(ctrPartial1(ip1))
+				//.addSegStrokeA(pp1[2].cx, pp1[2].cy)
 				.addSegStrokeA(pp1[3].cx, pp1[3].cy)
 				.addCornerRounded(param.Rrse)
 				//.addSegStrokeA(pp2[4].cx, pp2[4].cy)
@@ -436,7 +415,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 				.addSegArc2()
 				.addCornerRounded(param.Rrse)
 				.addSegStrokeA(pp2[5].cx, pp2[5].cy)
-				.addSegStrokeA(pp2[6].cx, pp2[6].cy)
+				.addPartial(ctrPartial2(ip2).rotate(0, 0, aSpringStep))
+				//.addSegStrokeA(pp2[6].cx, pp2[6].cy)
 				.addSegStrokeA(pp2[7].cx, pp2[7].cy)
 				.addCornerRounded(param.Rrsi)
 				//.closeSegStroke();
