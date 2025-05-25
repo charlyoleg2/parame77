baa207f81a916c8807f19f4bbdc78135048e27ba
charlyoleg <charles.braquet@gmx.fr>
2025-05-25 18:28:41 +0200

    contour of TopPole

--- pkg/desi77/src/heliostat/heliostat.ts
+++ pkg/desi77/src/heliostat/heliostat.ts
@@ -37,7 +37,7 @@ const pDef: tParamDef = {
 	partName: 'heliostat',
 	params: [
 		//pNumber(name, unit, init, min, max, step)
-		pNumber('L1', 'mm', 8000, 1000, 20000, 1),
+		pNumber('L1', 'mm', 4000, 1000, 20000, 1),
 		pNumber('D1', 'mm', 1000, 100, 4000, 1),
 		pNumber('D3', 'mm', 600, 100, 2000, 1),
 		pNumber('L9', 'mm', 8000, 1000, 20000, 1),
@@ -56,19 +56,20 @@ const pDef: tParamDef = {
 		pNumber('H1W', 'mm', 600, 100, 1000, 1),
 		pNumber('H1P', 'mm', 300, 100, 2000, 1),
 		pSectionSeparator('Junction'),
-		pNumber('Z3', 'mm', 300, 100, 2000, 1),
-		pNumber('Z4', 'mm', 300, 100, 2000, 1),
-		pSectionSeparator('Top details'),
-		pNumber('L4', 'mm', 300, 100, 2000, 1),
-		pNumber('E4', 'mm', 300, 100, 2000, 1),
-		pNumber('E5', 'mm', 300, 100, 2000, 1),
-		pNumber('E6', 'mm', 300, 100, 2000, 1),
-		pNumber('D6', 'mm', 300, 100, 2000, 1),
-		pNumber('H6', 'mm', 300, 100, 2000, 1),
-		pNumber('E7', 'mm', 300, 100, 2000, 1),
-		pNumber('D7', 'mm', 300, 100, 2000, 1),
+		pNumber('Z3', 'mm', 200, 1, 500, 1),
+		pNumber('Z4', 'mm', 200, 1, 500, 1),
+		pSectionSeparator('Top-trunk details'),
+		pNumber('L4', 'mm', 2000, 100, 20000, 1),
+		pNumber('E4', 'mm', 10, 1, 100, 1),
+		pNumber('E5', 'mm', 10, 1, 100, 1),
+		pNumber('H6', 'mm', 600, 100, 2000, 1),
+		pNumber('E7', 'mm', 10, 1, 100, 1),
+		pNumber('D7', 'mm', 200, 10, 2000, 1),
+		pSectionSeparator('Top-branch details'),
+		pNumber('E6', 'mm', 10, 1, 100, 1),
+		pNumber('D6', 'mm', 400, 100, 2000, 1),
 		pNumber('W8', 'mm', 300, 100, 2000, 1),
-		pNumber('L8', 'mm', 300, 100, 2000, 1)
+		pNumber('L8', 'mm', 6000, 100, 20000, 1)
 	],
 	paramSvg: {
 		L1: 'heliostat_bottom.svg',
@@ -111,6 +112,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const figBottomDisc = figure();
 	const figBottomPole = figure();
 	const figDoor = figure();
+	const figTopPole = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
@@ -124,12 +126,30 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		const S1b = param.E2 / Math.cos(pi2 - inclination);
 		const S1c = param.S1 + S1b;
 		const R1b = R1 - S1c;
+		const R1bb = R1 - S1b;
 		const S3b = param.E3 / Math.tan(inclination);
 		const S3c = param.S3 + S1b - S3b;
+		const R5 = R3 - S3c;
 		// fixation
 		const R2 = param.D2 / 2;
 		const lFixation = R1b + param.W1 + R2;
 		const aFixation = (2 * Math.PI) / param.N2;
+		// topPole
+		const d5b = param.Z3 / Math.tan(inclination);
+		const S5b = S3c + param.Z4 - d5b;
+		const S5t = S5b - param.E5 / Math.tan(inclination);
+		const d4b = param.L4 / Math.tan(inclination);
+		const R5b = R5 + S5b;
+		const R5t = R5 + S5t;
+		const R4i = R5b + d4b;
+		const E4b = param.E4 / Math.sin(inclination);
+		const R4e = R4i + E4b;
+		const posY4 = param.L1 + param.Z3 - param.L4;
+		const R7b = R5t - param.H6 / Math.tan(inclination);
+		const R7t = R7b - param.E7 / Math.tan(inclination) + E4b;
+		const R7i = param.D7 / 2;
+		//const S7b = R7b - R7i;
+		//const S7t = R7t - R7i;
 		//rGeome.logstr += `dbg082: ${S1b} ${S1c} ${R1b}\n`;
 		// step-5 : checks on the parameter values
 		if (R1 < R3) {
@@ -138,11 +158,22 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		if (R1b < 0.1) {
 			throw `err183: R1b ${ffix(R1b)} is too small because of ${ffix(param.E2)} or ${ffix(param.S1)} mm`;
 		}
+		if (param.S1 < 2 * (param.W1 + R2)) {
+			throw `err142: S1 ${ffix(param.S1)} is too small compare to W1 ${ffix(param.W1)} and D2 ${ffix(2 * R2)} mm`;
+		}
 		if (H1h < 0.1) {
 			throw `err167: H1H ${ffix(param.H1H)} is too small compare to H1W ${ffix(param.H1W)} mm`;
 		}
+		if (param.Z4 < d5b) {
+			throw `err154: Z4 ${ffix(param.Z4)} is too small compare to d5b ${ffix(d5b)} mm`;
+		}
+		if (R7b < R7i) {
+			throw `err169: R7b ${ffix(2 * R7b)} is too small because to D7 ${ffix(2 * R7i)} mm`;
+		}
 		// step-6 : any logs
 		rGeome.logstr += `Cone inclination: ${ffix(radToDeg(inclination))} deg\n`;
+		rGeome.logstr += `Diameters: D1 ${ffix(2 * R1)}, D1b ${ffix(2 * R1)}, D3 ${ffix(2 * R3)}, D5 ${ffix(2 * R5)} mm\n`;
+		rGeome.logstr += `TopPole: posY4 ${ffix(posY4)}, D4 ${ffix(2 * R4e)} mm\n`;
 		// step-7 : drawing of the figures
 		// sub-function
 		// figBottomDisc
@@ -156,17 +187,24 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			contourCircle(0, 0, R1b),
 			...fixationHoles
 		]);
+		figBottomDisc.addSecond(contourCircle(0, 0, R1bb));
+		figBottomDisc.addSecond(contourCircle(0, 0, R3));
+		figBottomDisc.addSecond(contourCircle(0, 0, R5));
 		// figBottomPole
-		const ctrBottomPole = contour(R1, 0)
-			.addSegStrokeR(0, param.E1)
-			.addSegStrokeR(R3 - R1, L1b)
-			.addSegStrokeR(-S3c, 0)
-			.addSegStrokeR(0, -param.E3)
-			.addSegStrokeR(param.S3, 0)
-			.addSegStrokeA(R1 - S1b, param.E1)
-			.addSegStrokeR(0, -param.E1)
-			.closeSegStroke();
-		figBottomPole.addMainO(ctrBottomPole);
+		function ctrBottomPole(sig: number): tContour {
+			const rCtr = contour(sig * R1, 0)
+				.addSegStrokeR(0, param.E1)
+				.addSegStrokeR(sig * (R3 - R1), L1b)
+				.addSegStrokeR(-sig * S3c, 0)
+				.addSegStrokeR(0, -param.E3)
+				.addSegStrokeR(sig * param.S3, 0)
+				.addSegStrokeA(sig * (R1 - S1b), param.E1)
+				.addSegStrokeR(0, -param.E1)
+				.closeSegStroke();
+			return rCtr;
+		}
+		figBottomPole.addMainO(ctrBottomPole(-1));
+		figBottomPole.addSecond(ctrBottomPole(1));
 		// figDoor
 		const ctrDoor = contour(H1W2, param.H1P + H1W2)
 			.addSegStrokeR(0, H1h)
@@ -178,11 +216,37 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			.addPointR(2 * H1W2, 0)
 			.addSegArc2();
 		figDoor.addMainO(ctrDoor);
+		figDoor.addSecond(ctrBottomPole(1));
+		figDoor.addSecond(ctrBottomPole(-1));
+		figBottomPole.addSecond(ctrDoor);
+		// figTopPole
+		function ctrTopPole(sig: number): tContour {
+			const rCtr = contour(-sig * R4e, posY4)
+				.addSegStrokeR(sig * E4b, 0)
+				.addSegStrokeR(sig * (R4i - R5b), param.L4)
+				.addSegStrokeR(sig * (R5b - R5), 0)
+				.addSegStrokeR(0, param.E5)
+				.addSegStrokeR(sig * (R5 - R5t), 0)
+				.addSegStrokeR(sig * (R5t - R7b), param.H6)
+				.addSegStrokeR(sig * (R7b - R7i), 0)
+				.addSegStrokeR(0, param.E7)
+				.addSegStrokeR(sig * (R7i - R7t), 0)
+				.closeSegStroke();
+			return rCtr;
+		}
+		figTopPole.addMainO(ctrTopPole(1));
+		figTopPole.addSecond(ctrTopPole(-1));
+		figTopPole.addSecond(ctrBottomPole(1));
+		figTopPole.addSecond(ctrBottomPole(-1));
+		figTopPole.addSecond(ctrDoor);
+		figBottomPole.addSecond(ctrTopPole(-1));
+		figBottomPole.addSecond(ctrTopPole(1));
 		// final figure list
 		rGeome.fig = {
-			faceBottomDisc: figBottomDisc,
 			faceBottomPole: figBottomPole,
-			faceDoor: figDoor
+			faceDoor: figDoor,
+			faceBottomDisc: figBottomDisc,
+			faceTopPole: figTopPole
 		};
 		// step-8 : recipes of the 3D construction
 		// volume
