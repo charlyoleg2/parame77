703b1c69a43595bee618266519654cb98ff0accb
charlyoleg <charles.braquet@gmx.fr>
2025-06-02 21:03:48 +0200

    platform done

--- pkg/desi77/src/rccar/rccar.ts
+++ pkg/desi77/src/rccar/rccar.ts
@@ -118,6 +118,7 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const figPlatform = figure();
 	const figTriangle = figure();
 	const figPFfixation = figure();
+	const figTop = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
@@ -132,6 +133,12 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		const platSurface = (Ltotal * param.W1) / 10 ** 6;
 		const R2 = param.D2 / 2;
 		const R1 = param.D1 / 2;
+		const LF2 = (param.L1 - param.F1) / 2;
+		const LF23 = LF2 + 2 * (param.T1 + param.E1);
+		const LF24 = LF23 + param.T1;
+		const F1b = param.F1 - 6 * param.T1 - 4 * param.E1;
+		const LF25 = LF24 + F1b;
+		const LF28 = LF25 + 2 * (param.T1 + param.E1);
 		const Rwheel = param.Dwheel / 2;
 		//const Raxis = param.Daxis / 2;
 		//const Rsteering = param.Dsteering / 2;
@@ -140,14 +147,20 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		const posZplatform = Rwheel + param.Z1 + param.L2 * Math.sin(A1x);
 		const Hplatform = posZplatform + param.H1;
 		// step-5 : checks on the parameter values
-		if (param.L1 < param.F1) {
+		if (LF2 < 0.1) {
 			throw `err176: L1 ${ffix(param.L1)} is too small compare to F1 ${ffix(param.F1)} mm`;
 		}
 		if (param.H1 < param.Z2 + param.T1) {
 			throw `err130: H1 ${ffix(param.H1)} is too small compare to Z2 ${ffix(param.Z2)} and T1 ${ffix(param.T1)} mm`;
 		}
-		if (param.Z2b < 0.1) {
+		if (Z2b < 0.1) {
 			throw `err149: Z2 ${ffix(param.Z2)} is too small compare to D2 ${ffix(param.D2)} mm`;
+		}
+		if (W2c < 0.1) {
+			throw `err153: W2 ${ffix(param.W2)} is too small compare to T1 ${ffix(param.T1)} mm`;
+		}
+		if (H1c < 0.1) {
+			throw `err156: H1c ${ffix(param.H1)} is too small compare to T1 ${ffix(param.T1)} mm`;
 		}
 		// step-6 : any logs
 		rGeome.logstr += `Platform Ltotal ${ffix(Ltotal / 1000)} m, surface ${ffix(platSurface)} m2\n`;
@@ -213,16 +226,49 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		for (const iCtr of makePFfixExt(-1)) {
 			figPlatform.addSecond(iCtr);
 		}
+		// figTop
+		figTop.addMainOI([
+			ctrRectangle(-W12, 0, param.W1, Ltotal),
+			ctrRectangle(-W22 + param.T1, 0, W2c, Ltotal)
+		]);
+		figTop.addSecond(ctrRectangle(-W22, 0, param.W2, Ltotal));
+		if (param.triangleExt) {
+			for (let ii = 0; ii < 2; ii++) {
+				const tPosY = ii * (param.L1 + param.T1) * param.N1;
+				figTop.addSecond(ctrRectangle(-W12, tPosY, W22b, param.T1));
+				figTop.addSecond(ctrRectangle(W22, tPosY, W22b, param.T1));
+			}
+		}
+		if (param.triangleInt) {
+			for (let ii = 1; ii < param.N1; ii++) {
+				const tPosY = ii * (param.L1 + param.T1);
+				figTop.addSecond(ctrRectangle(-W12, tPosY, W22b, param.T1));
+				figTop.addSecond(ctrRectangle(W22, tPosY, W22b, param.T1));
+			}
+		}
+		const tL2 = param.E2 + 2 * R2;
+		const tPosX2 = -W22 - tL2;
+		for (let ii = 0; ii < param.N1; ii++) {
+			const tPosY = param.T1 + ii * (param.L1 + param.T1);
+			for (const tPosY2 of [LF2, LF23, LF25, LF28]) {
+				const tPosY3 = tPosY + tPosY2;
+				figTop.addSecond(ctrRectangle(W22, tPosY3, tL2, param.T1));
+				figTop.addSecond(ctrRectangle(tPosX2, tPosY3, tL2, param.T1));
+			}
+		}
 		// final figure list
 		rGeome.fig = {
 			facePlatform: figPlatform,
 			faceTriangle: figTriangle,
-			facePFfixation: figPFfixation
+			facePFfixation: figPFfixation,
+			faceTop: figTop
 		};
 		// step-8 : recipes of the 3D construction
 		// volume
 		const designName = rGeome.partName;
+		const list3D: string[] = [];
 		const listPlatform: string[] = [];
+		const listBones: string[] = [];
 		const volPlatformMain: tExtrude = {
 			outName: `subpax_${designName}_platformMain`,
 			face: `${designName}_facePlatform`,
@@ -243,10 +289,30 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			};
 			return rVol;
 		}
-		volTriangle.push(extrudeTriangle(0, 0));
-		volTriangle.push(extrudeTriangle(Ltotal - param.T1, param.N1 + 1));
+		function extrudePFfixation(posZ: number, idx: number): tExtrude {
+			const rVol: tExtrude = {
+				outName: `subpax_${designName}_PFfixation${idx}`,
+				face: `${designName}_facePFfixation`,
+				extrudeMethod: EExtrude.eLinearOrtho,
+				length: param.T1,
+				rotate: [0, 0, 0],
+				translate: [0, 0, posZ]
+			};
+			return rVol;
+		}
+		for (let ii = 0; ii < param.N1; ii++) {
+			const tPosY = param.T1 + ii * (param.L1 + param.T1);
+			for (const [jj, tPosY2] of [LF2, LF23, LF25, LF28].entries()) {
+				const tPosY3 = tPosY + tPosY2;
+				const idx = 4 * ii + jj;
+				volTriangle.push(extrudePFfixation(tPosY3, idx));
+				listPlatform.push(`subpax_${designName}_PFfixation${idx}`);
+			}
+		}
 		listPlatform.push(`subpax_${designName}_platformMain`);
 		if (param.triangleExt) {
+			volTriangle.push(extrudeTriangle(0, 0));
+			volTriangle.push(extrudeTriangle(Ltotal - param.T1, param.N1 + 1));
 			listPlatform.push(`subpax_${designName}_platformTriangle${0}`);
 			listPlatform.push(`subpax_${designName}_platformTriangle${param.N1 + 1}`);
 		}
@@ -256,13 +322,20 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 				listPlatform.push(`subpax_${designName}_platformTriangle${ii + 1}`);
 			}
 		}
+		// list3D
+		if (param.gen3D === 0 || param.gen3D === 1) {
+			list3D.push(...listPlatform);
+		}
+		if (param.gen3D === 0 || param.gen3D === 2) {
+			list3D.push(...listBones);
+		}
 		rGeome.vol = {
 			extrudes: [volPlatformMain, ...volTriangle],
 			volumes: [
 				{
 					outName: `pax_${designName}`,
 					boolMethod: EBVolume.eUnion,
-					inList: [...listPlatform]
+					inList: list3D
 				}
 			]
 		};
