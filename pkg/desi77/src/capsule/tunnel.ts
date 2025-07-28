9819f5743da5c3e5625f79cf16a22ff279b85df1
charlyoleg <charles.braquet@gmx.fr>
2025-07-29 01:49:31 +0200

    first profile of tunnel

--- pkg/desi77/src/capsule/tunnel.ts
+++ pkg/desi77/src/capsule/tunnel.ts
@@ -16,11 +16,11 @@ import {
 	//withinPiPi,
 	//ShapePoint,
 	//point,
-	//contour,
+	contour,
 	contourCircle,
 	ctrRectangle,
 	figure,
-	//degToRad,
+	degToRad,
 	//radToDeg,
 	ffix,
 	pNumber,
@@ -66,41 +66,59 @@ const pDef: tParamDef = {
 
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
-	const figCylinder = figure();
-	const figHeight = figure();
+	const figProfile = figure();
+	const figSlice = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
-		const Ri = param.Di / 2;
-		const R1 = param.D1 / 2;
-		const surface = Math.PI * R1 ** 2 - Math.PI * Ri ** 2;
-		const volume = surface * param.T1;
+		const W12 = param.W1 / 2;
+		const a1 = degToRad(param.a1);
+		const sin = Math.sin(a1);
+		const cos = Math.cos(a1);
+		const kA = (W12 * (1 + cos)) / sin;
+		const kB = sin ** 2 / (1 + cos);
+		const H2 = (param.H1 - kA) * kB;
+		const R1 = (W12 * sin + H2 * cos) / sin ** 2;
+		const BC = R1 * cos;
+		const H1b = H2 + BC + R1;
+		const AD = H2 / Math.tan(a1);
+		const surf1 = R1 ** 2 * (Math.PI - a1);
+		const surf2 = (H2 + BC) * R1 * sin;
+		const surf3 = W12 * H2;
+		const surfaceM2 = (surf1 + surf2 + surf3) / 10 * 6;
 		// step-5 : checks on the parameter values
-		if (R1 < Ri) {
-			throw `err071: D1 ${param.D1} is too small compare to Di ${param.Di}`;
+		if (Math.abs(param.H1 - H1b) > 0.1) {
+			throw `err086: H1 ${param.H1} and H1b ${H1b} are not equal`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `Surface ${ffix(surface)} mm2, volume ${ffix(volume)} mm3\n`;
+		rGeome.logstr += `Surface ${ffix(surfaceM2)} m2\n`;
+		//rGeome.logstr += `dbg095: AD ${ffix(AD)} mm\n`;
 		// sub-function
-		// figCylinder
-		const ctrCylinder = contourCircle(0, 0, R1);
-		const ctrHole = contourCircle(0, 0, Ri);
-		figCylinder.addMainOI([ctrCylinder, ctrHole]);
-		// figHeight
-		figHeight.addMainO(ctrRectangle(-R1, 0, 2 * R1, param.T1));
-		figHeight.addSecond(ctrRectangle(-Ri, 0, 2 * Ri, param.T1));
+		// figProfile
+		const ctrProfileI = contour(-W12, 0)
+			.addSegStrokeR(2 * W12, 0)
+			.addSegStrokeR(AD, H2)
+			.addPointR(-AD - W12, BC + R1)
+			.addPointR(-2 * (AD + W12), 0)
+			.addSegArc2()
+			.closeSegStroke();
+		figProfile.addMainO(ctrProfileI);
+		figProfile.addSecond(contourCircle(0, H2 + BC, R1));
+		// figSlice
+		figSlice.addMainO(ctrRectangle(0, 0, param.T2, param.H1));
+		figSlice.addSecond(ctrRectangle(param.T2 + param.E1, 0, param.T2, param.H1));
 		// final figure list
 		rGeome.fig = {
-			faceCylinder: figCylinder,
-			faceHeight: figHeight
+			faceProfile: figProfile,
+			faceSlice: figSlice
 		};
 		// volume
 		const designName = rGeome.partName;
 		rGeome.vol = {
 			extrudes: [
 				{
-					outName: `subpax_${designName}_cyl`,
-					face: `${designName}_faceCylinder`,
+					outName: `subpax_${designName}_slice1`,
+					face: `${designName}_faceProfile`,
 					extrudeMethod: EExtrude.eLinearOrtho,
 					length: param.T1,
 					rotate: [0, 0, 0],
@@ -110,8 +128,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			volumes: [
 				{
 					outName: `pax_${designName}`,
-					boolMethod: EBVolume.eIdentity,
-					inList: [`subpax_${designName}_cyl`]
+					boolMethod: EBVolume.eUnion,
+					inList: [`subpax_${designName}_slice1`]
 				}
 			]
 		};
