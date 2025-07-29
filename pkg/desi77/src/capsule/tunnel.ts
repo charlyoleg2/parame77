b7d4ad62300b5dbd15d21db82535966a5fcd5dda
charlyoleg <charles.braquet@gmx.fr>
2025-07-29 02:14:29 +0200

    add profile exterior

--- pkg/desi77/src/capsule/tunnel.ts
+++ pkg/desi77/src/capsule/tunnel.ts
@@ -75,38 +75,63 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		const a1 = degToRad(param.a1);
 		const sin = Math.sin(a1);
 		const cos = Math.cos(a1);
-		const kA = (W12 * (1 + cos)) / sin;
-		const kB = sin ** 2 / (1 + cos);
-		const H2 = (param.H1 - kA) * kB;
-		const R1 = (W12 * sin + H2 * cos) / sin ** 2;
-		const BC = R1 * cos;
-		const H1b = H2 + BC + R1;
-		const AD = H2 / Math.tan(a1);
-		const surf1 = R1 ** 2 * (Math.PI - a1);
-		const surf2 = (H2 + BC) * R1 * sin;
-		const surf3 = W12 * H2;
-		const surfaceM2 = (surf1 + surf2 + surf3) / 10 * 6;
+		function calc(iW12: number, iH1: number): [number, number, number, number] {
+			const kA = (iW12 * (1 + cos)) / sin;
+			const kB = sin ** 2 / (1 + cos);
+			const H2 = (iH1 - kA) * kB;
+			const R1 = (iW12 * sin + H2 * cos) / sin ** 2;
+			const BC = R1 * cos;
+			const AD = H2 / Math.tan(a1);
+			return [H2, R1, BC, AD];
+		}
+		const [H2i, R1i, BCi, ADi] = calc(W12, param.H1);
+		const H1ib = H2i + BCi + R1i;
+		const fgox = param.T1 + param.E1;
+		const W12e = W12 + 2 * param.T1;
+		const H1e = param.H1 + 2 * param.T1;
+		const [H2e, R1e, BCe, ADe] = calc(W12e, H1e);
+		const H1eb = H2e + BCe + R1e;
+		const surf1 = R1i ** 2 * (Math.PI - a1);
+		const surf2 = (H2i + BCi) * R1i * sin;
+		const surf3 = W12 * H2i;
+		const surfaceM2 = (surf1 + surf2 + surf3) * 10 ** -6;
 		// step-5 : checks on the parameter values
-		if (Math.abs(param.H1 - H1b) > 0.1) {
-			throw `err086: H1 ${param.H1} and H1b ${H1b} are not equal`;
+		if (Math.abs(param.H1 - H1ib) > 0.1) {
+			throw `err086: H1 ${ffix(param.H1)} and H1ib ${ffix(H1ib)} are not equal`;
+		}
+		if (Math.abs(H1e - H1eb) > 0.1) {
+			throw `err102: H1e ${ffix(H1e)} and H1eb ${ffix(H1eb)} are not equal`;
 		}
 		// step-6 : any logs
 		rGeome.logstr += `Surface ${ffix(surfaceM2)} m2\n`;
-		//rGeome.logstr += `dbg095: AD ${ffix(AD)} mm\n`;
+		//rGeome.logstr += `dbg095: ADi ${ffix(ADi)} mm\n`;
 		// sub-function
 		// figProfile
 		const ctrProfileI = contour(-W12, 0)
 			.addSegStrokeR(2 * W12, 0)
-			.addSegStrokeR(AD, H2)
-			.addPointR(-AD - W12, BC + R1)
-			.addPointR(-2 * (AD + W12), 0)
+			.addSegStrokeR(ADi, H2i)
+			.addPointR(-ADi - W12, BCi + R1i)
+			.addPointR(-2 * (ADi + W12), 0)
 			.addSegArc2()
 			.closeSegStroke();
-		figProfile.addMainO(ctrProfileI);
-		figProfile.addSecond(contourCircle(0, H2 + BC, R1));
+		const ctrProfileE = contour(-W12e, -param.T1)
+			.addSegStrokeR(2 * W12e, 0)
+			.addSegStrokeR(ADe, H2e)
+			.addPointR(-ADe - W12e, BCe + R1e)
+			.addPointR(-2 * (ADe + W12e), 0)
+			.addSegArc2()
+			.closeSegStroke();
+		figProfile.addMainOI([ctrProfileE, ctrProfileI]);
+		figProfile.addSecond(contourCircle(0, H2i + BCi, R1i));
 		// figSlice
-		figSlice.addMainO(ctrRectangle(0, 0, param.T2, param.H1));
-		figSlice.addSecond(ctrRectangle(param.T2 + param.E1, 0, param.T2, param.H1));
+		figSlice.addMainOI([
+			ctrRectangle(0, -param.T1, param.T2, param.H1 + 2 * param.T2),
+			ctrRectangle(0, 0, param.T2, param.H1)
+		]);
+		figSlice.addMainOI([
+			ctrRectangle(fgox, -param.T1, param.T2, param.H1 + 2 * param.T2),
+			ctrRectangle(fgox, 0, param.T2, param.H1)
+		]);
 		// final figure list
 		rGeome.fig = {
 			faceProfile: figProfile,
