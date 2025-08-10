bd18712a6fd5c0d2c75efaea2e4d01c033fd9e5f
charlyoleg <charles.braquet@gmx.fr>
2025-08-11 00:54:25 +0200

    frame-mid

--- pkg/desi77/src/heliostat/minihelio.ts
+++ pkg/desi77/src/heliostat/minihelio.ts
@@ -18,7 +18,7 @@ import {
 	//point,
 	contour,
 	contourCircle,
-	//ctrRectangle,
+	ctrRectangle,
 	figure,
 	//degToRad,
 	//radToDeg,
@@ -56,12 +56,12 @@ const pDef: tParamDef = {
 		pNumber('HW3', 'mm', 40, 1, 2000, 1),
 		pNumber('HW53', 'mm', 60, 1, 2000, 1),
 		pSectionSeparator('Foot'),
-		pNumber('D1', 'mm', 10, 5, 1000, 1),
-		pNumber('D2', 'mm', 20, 5, 1000, 1),
-		pNumber('D3', 'mm', 10, 5, 1000, 1),
-		pNumber('H1', 'mm', 10, 5, 2000, 1),
-		pNumber('H2', 'mm', 20, 5, 2000, 1),
-		pNumber('H3', 'mm', 40, 5, 2000, 1),
+		pNumber('D1', 'mm', 30, 5, 1000, 1),
+		pNumber('D2', 'mm', 40, 5, 1000, 1),
+		pNumber('D3', 'mm', 20, 5, 1000, 1),
+		pNumber('H1', 'mm', 20, 5, 2000, 1),
+		pNumber('H2', 'mm', 40, 5, 2000, 1),
+		pNumber('H3', 'mm', 80, 5, 2000, 1),
 		pSectionSeparator('Mirror'),
 		pNumber('Wm', 'mm', 60, 5, 2000, 1),
 		pNumber('Hm', 'mm', 60, 5, 2000, 1),
@@ -117,6 +117,11 @@ const pDef: tParamDef = {
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
 	const figFoot = figure();
+	const figFrameBand = figure();
+	const figFrameBottom = figure();
+	const figFrameTop = figure();
+	const figFrameSide = figure();
+	const figFrameMid = figure();
 	const figMirrorSide = figure();
 	const figMirrorAxis = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
@@ -128,6 +133,17 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		const Rm = param.Dm / 2;
 		const R12 = R2 - R1;
 		const R23 = R2 - R3;
+		const Wbottom2 = param.W1 / 2 - param.H4;
+		const H123 = param.H1 + param.H2 + param.H3;
+		const H567 = param.H5 + param.T2 + param.N1 * (param.H6 + param.T2) + param.H7;
+		const W12 = param.W1 / 2;
+		const W1b = param.W1 - 2 * param.T2;
+		const H15 = H123 + param.H4 + param.H5;
+		const H6b = param.H6 + param.T2;
+		const H18pre = H15 + param.T2 + param.H8;
+		const H18b = H18pre - Rm;
+		const H18 = H18pre - param.Hm / 2;
+		const L8 = (param.W1 - param.Wm) / 2;
 		const Hm2 = param.Hm / 2 - Rm;
 		const mirrorSurface = param.Hm * param.Wm;
 		const mirrorSurfaceN = param.N1 * mirrorSurface;
@@ -139,7 +155,13 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			throw `err139: D2 ${ffix(param.D2)} is too small compare to D3 ${ffix(param.D3)} mm`;
 		}
 		if (param.H2 < param.T1) {
-			throw `err142: H2 ${ffix(param.h2)} is too small compare to T1 ${ffix(param.T1)} mm`;
+			throw `err142: H2 ${ffix(param.H2)} is too small compare to T1 ${ffix(param.T1)} mm`;
+		}
+		if (Wbottom2 < R3) {
+			throw `err151: W1 ${ffix(param.W1)} is too small compare to H4 ${ffix(param.H4)} mm`;
+		}
+		if (W1b < 0) {
+			throw `err158: W1 ${ffix(param.W1)} is too small compare to T2 ${ffix(param.T2)} mm`;
 		}
 		if (Hm2 < 0) {
 			throw `err096: Hm ${ffix(param.Hm)} is too small compare to Dm ${ffix(param.Dm)} mm`;
@@ -153,7 +175,6 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		// step-6 : any logs
 		rGeome.logstr += `Mirror surface: One: ${ffix(mirrorSurface)}, N: ${param.N1} : ${ffix(mirrorSurfaceN)} mm2\n`;
 		// sub-function
-		// figFoot
 		function ctrFoot(sign: number): tContour {
 			const rCtr = contour(sign * R1, 0)
 				.addSegStrokeR(0, param.H1)
@@ -168,8 +189,50 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 				.closeSegStroke();
 			return rCtr;
 		}
+		function ctrOval(iDelta: number): tContour {
+			const r4 = param.H4 - iDelta;
+			const r1 = W12 - iDelta;
+			const rCtr = contour(Wbottom2, H123 + iDelta)
+				.addPointR(r4, r4)
+				.addSegArc(r4, false, true)
+				.addSegStrokeR(0, H567)
+				.addPointR(-r1, r1)
+				.addPointR(-2 * r1, 0)
+				.addSegArc2()
+				.addSegStrokeR(0, -H567)
+				.addPointR(r4, -r4)
+				.addSegArc(r4, false, true)
+				.closeSegStroke();
+			return rCtr;
+		}
+		// figFoot
 		figFoot.addMainO(ctrFoot(1));
 		figFoot.addSecond(ctrFoot(-1));
+		figFoot.addSecond(ctrOval(0));
+		figFoot.addSecond(ctrOval(param.T2));
+		figFoot.addSecond(ctrOval(param.T2 + param.W2));
+		// figFrameBand
+		figFrameBand.addMainOI([ctrOval(param.T2), ctrOval(param.T2 + param.W2)]);
+		figFrameBand.addSecond(ctrOval(0));
+		figFrameBand.addSecond(ctrFoot(1));
+		figFrameBand.addSecond(ctrFoot(-1));
+		// figFrameMid
+		figFrameMid.mergeFigure(figFoot, true);
+		for (let ii = 0; ii < param.N1 + 1; ii++) {
+			figFrameMid.addMainO(ctrRectangle(-W1b / 2, H15 + ii * H6b, W1b, param.T2));
+		}
+		for (let ii = 0; ii < param.N1; ii++) {
+			figFrameMid.addSecond(ctrRectangle(-param.Wm / 2, H18 + ii * H6b, param.Wm, param.Hm));
+			figFrameMid.addSecond(ctrRectangle(-param.W1 / 2, H18b + ii * H6b, L8, param.Dm));
+			figFrameMid.addSecond(ctrRectangle(param.Wm / 2, H18b + ii * H6b, L8, param.Dm));
+		}
+		// figFrameTop
+		figFrameTop.mergeFigure(figFrameMid, true);
+		// figFrameBottom
+		figFrameBottom.mergeFigure(figFrameMid, true);
+		// figFrameSide
+		figFrameSide.addSecond(ctrFoot(1));
+		figFrameSide.addSecond(ctrFoot(-1));
 		// figMirrorSide
 		const ctrMirrorSide = contour(-Rm, 0)
 			.addPointR(Rm, -Rm)
@@ -191,6 +254,11 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		// final figure list
 		rGeome.fig = {
 			faceFoot: figFoot,
+			faceFrameBand: figFrameBand,
+			faceFrameTop: figFrameTop,
+			faceFrameBottom: figFrameBottom,
+			faceFrameSide: figFrameSide,
+			faceFrameMid: figFrameMid,
 			faceMirrorSide: figMirrorSide,
 			faceMirrorAxis: figMirrorAxis
 		};
