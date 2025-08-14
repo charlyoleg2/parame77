452f0510dd11b8ecb326743f07d9e13fcf703d32
charlyoleg <charles.braquet@gmx.fr>
2025-08-14 03:27:30 +0200

    bridge params

--- pkg/desi77/src/capsule/bridge.ts
+++ pkg/desi77/src/capsule/bridge.ts
@@ -16,16 +16,16 @@ import {
 	//withinPiPi,
 	//ShapePoint,
 	//point,
-	//contour,
-	contourCircle,
-	ctrRectangle,
+	contour,
+	//contourCircle,
+	//ctrRectangle,
 	figure,
 	//degToRad,
 	//radToDeg,
 	ffix,
 	pNumber,
 	//pCheckbox,
-	//pDropdown,
+	pDropdown,
 	pSectionSeparator,
 	EExtrude,
 	EBVolume,
@@ -38,15 +38,63 @@ const pDef: tParamDef = {
 	partName: 'bridge',
 	params: [
 		//pNumber(name, unit, init, min, max, step)
-		pNumber('D1', 'mm', 100, 5, 500, 1),
-		pNumber('Di', 'mm', 50, 2, 500, 1),
-		pSectionSeparator('Thickness'),
-		pNumber('T1', 'mm', 30, 1, 500, 1)
+		pNumber('N1', 'arc', 10, 1, 100, 1),
+		pNumber('W1', 'm', 20, 1, 100, 0.1),
+		pNumber('W2', 'm', 2, 1, 100, 0.1),
+		pNumber('H1', 'm', 2, 1, 100, 0.1),
+		pNumber('H2', 'm', 30, 1, 100, 0.1),
+		pDropdown('arcStyle', ['rectangular', 'half-circle', 'gothic']),
+		pNumber('Ea', 'm', 10, 1, 100, 0.1),
+		pSectionSeparator('Transversal'),
+		pNumber('W3', 'm', 10, 1, 100, 1),
+		pNumber('W4', 'm', 30, 1, 100, 1),
+		pNumber('H3', 'm', 5, 1, 500, 1),
+		pNumber('H4', 'm', 20, 1, 500, 1),
+		pDropdown('sideStyle', ['straight', 'arc']),
+		pSectionSeparator('Valley extremity'),
+		pNumber('Ix1', 'm', 5, 0, 50, 0.1),
+		pNumber('Ih11', 'm', 2, 1, 50, 1),
+		pNumber('Ih12', 'm', 2, 1, 50, 1),
+		pNumber('Ix2', 'm', 5, 0, 50, 0.1),
+		pNumber('Ih21', 'm', 2, 1, 50, 1),
+		pNumber('Ih22', 'm', 2, 1, 50, 1),
+		pSectionSeparator('Valley'),
+		pNumber('p1x', '%', 20, 1, 99, 1),
+		pNumber('p1y', 'm', -10, -300, 10, 1),
+		pNumber('p2x', '%', 40, 1, 99, 1),
+		pNumber('p2y', 'm', -20, -300, 10, 1),
+		pNumber('p3x', '%', 60, 1, 99, 1),
+		pNumber('p3y', 'm', -25, -300, 10, 1),
+		pNumber('p4x', '%', 80, 1, 99, 1),
+		pNumber('p4y', 'm', -15, -300, 10, 1)
 	],
 	paramSvg: {
-		D1: 'bridge_panorama.svg',
-		Di: 'bridge_column.svg',
-		T1: 'bridge_panorama.svg'
+		N1: 'bridge_panorama.svg',
+		W1: 'bridge_panorama.svg',
+		W2: 'bridge_panorama.svg',
+		H1: 'bridge_panorama.svg',
+		H2: 'bridge_panorama.svg',
+		arcStyle: 'bridge_arc.svg',
+		Ea: 'bridge_column.svg',
+		W3: 'bridge_column.svg',
+		W4: 'bridge_column.svg',
+		H3: 'bridge_column.svg',
+		H4: 'bridge_column.svg',
+		sideStyle: 'bridge_column.svg',
+		Ix1: 'bridge_panorama.svg',
+		Ih11: 'bridge_panorama.svg',
+		Ih12: 'bridge_panorama.svg',
+		Ix2: 'bridge_panorama.svg',
+		Ih21: 'bridge_panorama.svg',
+		Ih22: 'bridge_panorama.svg',
+		p1x: 'bridge_panorama.svg',
+		p1y: 'bridge_panorama.svg',
+		p2x: 'bridge_panorama.svg',
+		p2y: 'bridge_panorama.svg',
+		p3x: 'bridge_panorama.svg',
+		p3y: 'bridge_panorama.svg',
+		p4x: 'bridge_panorama.svg',
+		p4y: 'bridge_panorama.svg'
 	},
 	sim: {
 		tMax: 180,
@@ -57,43 +105,52 @@ const pDef: tParamDef = {
 
 function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	const rGeome = initGeom(pDef.partName + suffix);
-	const figCylinder = figure();
-	const figHeight = figure();
+	const figArcade = figure();
+	const figColumn = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
-		const Ri = param.Di / 2;
-		const R1 = param.D1 / 2;
-		const surface = Math.PI * R1 ** 2 - Math.PI * Ri ** 2;
-		const volume = surface * param.T1;
+		const W12 = param.W1 / 2;
+		const W32 = param.W3 / 2;
+		const W42 = param.W4 / 2;
+		const H12 = param.H1 + param.H2;
+		const La = param.W2 + param.N1 * (param.W1 + param.W2);
+		const Lb = La - param.Ix1 - param.Ix2;
 		// step-5 : checks on the parameter values
-		if (R1 < Ri) {
-			throw `err071: D1 ${param.D1} is too small compare to Di ${param.Di}`;
+		if (param.H2 < W12 && param.arcStyle === 1) {
+			throw `err116: H2 ${param.H2} is too small compare to W1 ${param.W1}`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `Surface ${ffix(surface)} mm2, volume ${ffix(volume)} mm3\n`;
+		rGeome.logstr += `Bridge length: ${ffix(Lb)} m\n`;
 		// sub-function
-		// figCylinder
-		const ctrCylinder = contourCircle(0, 0, R1);
-		const ctrHole = contourCircle(0, 0, Ri);
-		figCylinder.addMainOI([ctrCylinder, ctrHole]);
-		// figHeight
-		figHeight.addMainO(ctrRectangle(-R1, 0, 2 * R1, param.T1));
-		figHeight.addSecond(ctrRectangle(-Ri, 0, 2 * Ri, param.T1));
+		// figArcade
+		const ctrArcade = contour(0, 0)
+			.addSegStrokeR(0, -H12)
+			.addSegStrokeR(La, 0)
+			.addSegStrokeR(0, H12)
+			.closeSegStroke();
+		figArcade.addMainO(ctrArcade);
+		// figColumn
+		const ctrColumn = contour(-W32, 0)
+			.addSegStrokeR(W32 - W42, -H12)
+			.addSegStrokeR(2 * W42, 0)
+			.addSegStrokeR(0, H12)
+			.closeSegStroke();
+		figColumn.addMainO(ctrColumn);
 		// final figure list
 		rGeome.fig = {
-			faceCylinder: figCylinder,
-			faceHeight: figHeight
+			faceArcade: figArcade,
+			faceColumn: figColumn
 		};
 		// volume
 		const designName = rGeome.partName;
 		rGeome.vol = {
 			extrudes: [
 				{
-					outName: `subpax_${designName}_cyl`,
-					face: `${designName}_faceCylinder`,
+					outName: `subpax_${designName}_arcade`,
+					face: `${designName}_faceArcade`,
 					extrudeMethod: EExtrude.eLinearOrtho,
-					length: param.T1,
+					length: param.W3,
 					rotate: [0, 0, 0],
 					translate: [0, 0, 0]
 				}
@@ -101,8 +158,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			volumes: [
 				{
 					outName: `pax_${designName}`,
-					boolMethod: EBVolume.eIdentity,
-					inList: [`subpax_${designName}_cyl`]
+					boolMethod: EBVolume.eUnion,
+					inList: [`subpax_${designName}_arcade`]
 				}
 			]
 		};
